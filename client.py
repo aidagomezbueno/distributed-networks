3e77371d5602a4655efdbe328524cee4d1ac68d9
Aida Gomezbueno Berezo <71767272+aidagomezbueno@users.noreply.github.com>
2024-03-04 13:25:46 +0100

    20240304 server and client implementations

---
+++ client.py
@@ -0,0 +1,83 @@
+import socket
+import threading
+from tkinter import *
+from tkinter import simpledialog
+import json
+
+class ChatClient:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Chat Room")
+        
+        # Prompt for server IP, port, and username
+        self.server_ip = simpledialog.askstring("Server IP", "Enter the server's IP:", parent=self.master)
+        self.server_port = simpledialog.askinteger("Server Port", "Enter the server's port:", parent=self.master)
+        self.username = simpledialog.askstring("Username", "Enter your username:", parent=self.master)
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.server_ip, self.server_port))
+        
+        # Send the username to the server right after connecting
+        self.client_socket.send(self.username.encode('utf-8'))
+        
+        # Setup the GUI
+        self.setup_gui()
+        
+        # Start the thread to listen for incoming messages
+        threading.Thread(target=self.receive_message, daemon=True).start()
+
+    def setup_gui(self):
+        # Setup the main chat window
+        self.text_area = Text(self.master, state='disabled')
+        self.text_area.pack(padx=20, pady=10)
+        self.msg_entry = Entry(self.master)
+        self.msg_entry.pack(padx=20, pady=10)
+        self.msg_entry.bind("<Return>", self.send_message)
+        self.send_button = Button(self.master, text="Send", command=self.send_message)
+        self.send_button.pack(pady=5)
+        # self.text_area.tag_configure('join', foreground='grey')
+        # self.text_area.tag_configure('leave', foreground='grey')
+        self.text_area.tag_configure('grey', foreground='grey', justify='center')
+        # self.text_area.tag_configure('normal')
+
+    def send_message(self, event=None):
+        message = self.msg_entry.get()
+        self.msg_entry.delete(0, END)
+        if message.lower() == 'quit':
+            self.client_socket.send('quit'.encode('utf-8'))
+            self.client_socket.close()
+            self.master.quit()
+        else:
+            self.client_socket.send(message.encode('utf-8'))
+
+    def receive_message(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode('utf-8')
+                if message == '':
+                    break
+                self.display_message(message)
+            except Exception as e:
+                print("Connection to server lost:", e)
+                break
+
+
+    def display_message(self, message):
+        self.text_area.config(state='normal')
+        if "has joined the chat!" in message or "You're currently alone in this room." in message or "has left the chat." in message:
+            self.text_area.insert(END, message + "\n", 'grey')
+        else:
+            self.text_area.insert(END, message + "\n")
+        self.text_area.config(state='disabled')
+        self.text_area.yview(END)
+        # self.text_area.config(state='normal')
+        # if "has joined the chat!" in message or "You're currently alone in this room." in message:
+        #     self.text_area.insert(END, message + "\n", 'grey')
+        # else:
+        #     self.text_area.insert(END, message + "\n")
+        # self.text_area.config(state='disabled')
+        # self.text_area.yview(END)
+
+root = Tk()
+app = ChatClient(root)
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,73 @@
+import socket
+import threading
+import signal
+import sys
+
+clients = []
+client_info = {}  # To keep track of client addresses and usernames
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def broadcast_message(message, sender_socket=None):
+    for client_socket in clients:
+        # if client_socket != sender_socket:
+        try:
+            client_socket.send(message.encode('utf-8'))
+        except:
+            clients.remove(client_socket)
+            client_socket.close()
+
+def handle_client(client_socket, client_address):
+    username = client_socket.recv(1024).decode('utf-8')
+    welcome_message = f"\n\n{username} has joined the chat!"
+    broadcast_message(welcome_message)
+    client_info[client_socket] = username
+
+    # Send the list of connected users to the newly connected client
+    connected_users = [info for sock, info in client_info.items() if sock != client_socket]
+    if len(connected_users) > 0:
+        client_socket.send(f"\n\nConnected users: {', '.join(connected_users)}".encode('utf-8'))
+    else:
+        client_socket.send(f"\n\nYou're currently alone in this room.".encode('utf-8'))
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if message.lower() == 'quit':
+                break
+            else:
+                broadcast_message(f"{username}: {message}", client_socket)
+        except:
+            break
+
+    # Client has disconnected
+    clients.remove(client_socket)
+    broadcast_message(f"\n\n{username} has left the chat.\n\n")
+    del client_info[client_socket]
+    client_socket.close()
+
+def start_server():
+    server_socket.bind(('127.0.0.1', 12345))
+    server_socket.listen()
+    print("Server started, waiting for connections...")
+
+    while True:
+        try:
+            client_socket, client_address = server_socket.accept()
+            print(f"Connection from {client_address}")
+            clients.append(client_socket)
+            thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            thread.start()
+        except OSError:  # Socket is closed, exit the loop
+            break
+
+def signal_handler(sig, frame):
+    print("Shutting down server...")
+    for client_socket in clients:
+        client_socket.close()
+    server_socket.close()
+    sys.exit(0)
+
+# Attach the signal handler for SIGINT (Ctrl+C)
+signal.signal(signal.SIGINT, signal_handler)
+
+start_server()
